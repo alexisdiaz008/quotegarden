e27da6f084b776b66ea296bfd70eb86437b1216f
Lex <lexmasta@gmail.com>
2016-05-08 14:37:12 -0400

    changed root route

--- certifyed3d.rb
+++ certifyed3d.rb
@@ -2,5 +2,6 @@ require 'sinatra'
 
 get '/' do
   # File.open('index.html')
-  send_file 'index.html', :type => 'text/html; charset=utf-8'
+  # send_file 'index.html', :type => 'text/html; charset=utf-8'
+  File.read(File.join('public', 'index.html'))
 end
