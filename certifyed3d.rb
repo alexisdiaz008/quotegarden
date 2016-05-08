cf9d037c394435dd354833c7cef54163a3a8ef55
Lex <lexmasta@gmail.com>
2016-05-08 14:49:58 -0400

    modify root

--- certifyed3d.rb
+++ certifyed3d.rb
@@ -3,5 +3,5 @@ require 'sinatra'
 get '/' do
   # File.open('index.html')
   # send_file 'index.html', :type => 'text/html; charset=utf-8'
-  File.read(File.join('public', 'index.html'))
+  File.read(File.join('html', 'index.html'))
 end
