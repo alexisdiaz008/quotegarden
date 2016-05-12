e312d71fc39de481b0fd2d5cfb2a8d44291ca721
Lex <lexmasta@gmail.com>
2016-05-12 01:42:33 -0400

    added quotes and beginning of google image search

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 gem 'rack'
 gem 'sinatra'
-gem 'quote'
-
+gem 'httparty'
+gem 'google_custom_search_api'
 group :development do
   gem 'better_errors'
   # uncomment this for more advanced features:
--- quotegarden.rb
+++ quotegarden.rb
@@ -1,5 +1,26 @@
 require 'sinatra'
+require 'httparty'
+require 'google_custom_search_api'
+
+configure :development do
+require 'better_errors'
+  use BetterErrors::Middleware
+  # you need to set the application root in order to abbreviate filenames
+  # within the application:
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
 
 get '/' do
-  File.read(File.join('index.html'))
+  # @quote_hash=JSON.parse(HTTParty.get('http://quotesondesign.com/api/3.0/api-3.0.json'))
+  # @quote = HTTParty.get('https://healthruwords.p.mashape.com/v1/quotes/')
+  @quote = JSON.parse(HTTParty.get('https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies', 
+    :headers => {
+    "X-Mashape-Key" => "L8mkOYD71DmshNiPAlvv8nSaelynp1KLQtMjsnZlIRIx0lL3Yc",
+    "Content-Type" => "application/x-www-form-urlencoded",
+    "Accept" => "application/json"
+  }))
+  results = GoogleCustomSearchApi.search(@quote['author'])
+  raise
+  # File.read(File.join('index.html'))
+  erb :index
 end
